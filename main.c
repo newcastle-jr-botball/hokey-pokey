555a4f0b6e6ccea06e37d6e29977bd179321566e
newcastle-jr-botball <brianscottwatkins@gmail.com>
2015-03-11 11:28:12 -0500

    building in the function bodies - still need to document code - small bit of refactoring

--- main.c
+++ main.c
@@ -13,10 +13,12 @@ robot (powered by the KIPR Link -- http://www.kipr.org/hardware-software) to do 
 
 // now let's declare our functions
 int doTheHokeyPokey();
-void moveWheel(direction, wheel);
-void shakeWheel(wheel);
-void turnYourselfAround();
-void printMessage(msg);
+void moveWheel(int direction, int wheel);
+void moveWheels(int direction);
+void shakeWheel(int wheel);
+void shakeWheels();
+void turnYourselfAround(int wheel);
+void printMessage();
 void haveSomeFun();
 
 // When we touch the "Run" button on the Link, this function is called. This is the entry point 
@@ -43,10 +45,10 @@ int doTheHokeyPokey()
 	shakeWheel(RIGHT_WHEEL);
 	
 	// you do the hokey pokey and you turn yourself around
-	turnYourselfAround();
+	turnYourselfAround(RIGHT_WHEEL);
 	
 	// That's what it's all about!
-	printMessage("That's what it's all about!");
+	printMessage();
 	
 	// You do the Hooooookey Pokey!
 	haveSomeFun();
@@ -67,10 +69,10 @@ int doTheHokeyPokey()
 	shakeWheel(LEFT_WHEEL);
 	
 	// you do the hokey pokey and you turn yourself around
-	turnYourselfAround();
+	turnYourselfAround(LEFT_WHEEL);
 	
 	// That's what it's all about!
-	printMessage("That's what it's all about!");
+	printMessage();
 	
 	// You do the Hooooookey Pokey!
 	haveSomeFun();
@@ -79,33 +81,132 @@ int doTheHokeyPokey()
 	haveSomeFun();
 	
 	// You put your whole self in
-	moveWheel(1, RIGHT_WHEEL);
-	moveWheel(1, LEFT_WHEEL);
+	moveWheels(1);
 	
 	// You put your whole self out
-	moveWheel(-1, RIGHT_WHEEL);
-	moveWheel(-1, LEFT_WHEEL);
+	moveWheels(-1);
 	
 	// You put your whole self in
-	moveWheel(1, RIGHT_WHEEL);
-	moveWheel(1, LEFT_WHEEL);
+	moveWheels(1);
 	
 	// and you shake it all about
-	shakeWheel(RIGHT_WHEEL);
-	shakeWheel(LEFT_WHEEL);
+	shakeWheels();
 	
 	// you do the hokey pokey and you turn yourself around
-	turnYourselfAround();
+	turnYourselfAround(LEFT_WHEEL);
 	
 	// That's what it's all about!
-	printMessage("That's what it's all about!");
-	
-	// You do the Hooooookey Pokey!
-	haveSomeFun();
-	
-	// You do the Hooooookey Pokey!
-	haveSomeFun();
+	printMessage();
 	
 	// then we're done!
 	return 0;
 }
+
+/**
+ * TODO: document this function
+ */
+void moveWheel(int direction, int wheel)
+{
+	char *dir = "in";
+	char *wh = "left wheel";
+	if (direction < 0) {
+		dir = "out";
+	}
+	if (wheel == RIGHT_WHEEL) {
+		wh = "right wheel";
+	}
+	printf("You put your %s %s\n", wh, dir);
+	motor(wheel, 25 * direction);
+	msleep(1000);
+	off(wheel);
+}
+
+/**
+ * TODO: document this function
+ */
+void moveWheels(int direction) 
+{
+	char *dir = "in";
+	if (direction < 0) {
+		dir = "out";
+	}
+	printf("You put your whole self %s\n", dir);
+	motor(RIGHT_WHEEL, 25 * direction);
+	motor(LEFT_WHEEL, 25 * direction);
+	msleep(1000);
+	off(RIGHT_WHEEL);
+	off(LEFT_WHEEL);
+}
+
+/**
+ * TODO: document this function
+ */
+void shakeWheel(int wheel)
+{
+	printf("and you shake it all about.\n");
+	for (int i = 0; i < 5; ++i) 
+	{
+		motor(wheel, 100);
+		msleep(100);
+		motor(wheel, -100);
+		msleep(100);
+	}
+	motor(wheel, 100);
+	msleep(100);
+	off(wheel);
+}
+
+/**
+ * TODO: document this function
+ */
+void shakeWheels()
+{
+	printf("and you shake it all about.\n");
+	for (int i = 0; i < 5; ++i) 
+	{
+		motor(RIGHT_WHEEL, 100);
+		motor(LEFT_WHEEL, -100);
+		msleep(100);
+		motor(RIGHT_WHEEL, -100);
+		motor(LEFT_WHEEL, 100);
+		msleep(100);
+	}
+	motor(RIGHT_WHEEL, 100);
+	motor(LEFT_WHEEL, -100);
+	msleep(100);
+	off(RIGHT_WHEEL);
+	off(LEFT_WHEEL);
+}
+
+/**
+ * TODO: document this function
+ */
+void turnYourselfAround(int wheel)
+{
+	printf("You do the Hokey Pokey\n");
+	printf("and you turn yourself around\n");
+	motor(wheel, 100);
+	msleep(2500);
+	off(wheel);
+}
+
+/**
+ * TODO: document this function
+ */
+void printMessage()
+{
+	printf("That's what it's all about!\n");
+	msleep(1000);
+}
+
+/**
+ * TODO: document this function
+ */
+void haveSomeFun()
+{
+	printf("You do the Hoooooookey Pokey!\n");
+	motor(RIGHT_WHEEL, 100);
+	msleep(2500);
+	off(RIGHT_WHEEL);
+}
+
